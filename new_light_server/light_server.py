2f5a8d8a638ad4dbe789aedebb3f2a2dbfaa30ca
Robert Diamond <rmd6502@gmail.com>
2014-05-26 19:56:01 -0400

    add set

--- new_light_server/light_server.py
+++ new_light_server/light_server.py
@@ -14,19 +14,34 @@ import time
 import logging
 
 logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.WARN)
 ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
+ch.setLevel(logging.WARN)
 logger.addHandler(ch)
 
 app = Flask(__name__)
 nodes = []
 xbee = None
 
+def sendToNode(node,data,frame_id='A'):
+    logger.debug("sending %s to %s", data, node['string_address'])
+    xbee.send("tx", dest_addr_long=node['address'], dest_addr='\xff\xfe', data=data, frame_id=frame_id)
+
 @app.route('/')
 @app.route('/lights')
 def index():
-    logger.debug(request.form)
+    logger.debug(request.args.to_dict())
+    for node in nodes:
+        key=node['name']+"_color"
+        print key
+        if key in request.args:
+            print 'got it'
+            # this can probably be done more simply
+            newcolor = struct.unpack('4B',struct.pack('>L',int(request.args[key],16)))[1:]
+            if 'color' not in node or node['color'] != newcolor:
+                node['color'] = newcolor
+                node['colorvalue'] = request.args[key]
+                sendToNode(node,'r{0}g{1}b{2}\n'.format(newcolor[0],newcolor[1],newcolor[2]))
     return render_template('lights.html',nodes=nodes)
 
 @app.route('/query')
@@ -35,15 +50,14 @@ def query():
     for node in nodes:
         retnode = {'address': node['string_address'], 'name': node['name']}
         if 'color' in node:
-            retnode['color'] = node['color']
+            retnode['color'] = '{0[0]:02x}{0[1]:02x}{0[2]:02x}'.format(node['color'])
         retnodes.append(retnode)
     return jsonify(nodes=retnodes)
-
 
 def add_node(data): 
     logger.debug('%s',data)
     if data['id'] == 'at_response' and data['command'] == 'ND':
-        nodes.append({'address': data['parameter']['source_addr_long'], 'string_address': '{0:08x}'.format(struct.unpack('>Q',data['parameter']['source_addr_long'])[0]), 'name': data['parameter']['node_identifier']})
+        nodes.append({'address': data['parameter']['source_addr_long'], 'string_address': '{0:08x}'.format(struct.unpack('>Q',data['parameter']['source_addr_long'])[0]), 'name': data['parameter']['node_identifier'], 'colorvalue': '000000'})
 
 
 def do_queries():
@@ -51,7 +65,7 @@ def do_queries():
     while True:
         for node in nodes:
             logger.debug("Querying %s", node['string_address'])
-            xbee.send("tx", dest_addr_long=node['address'], dest_addr='\xff\xfe', data='AQ', frame_id='2')
+            sendToNode(node,'AQ')
         time.sleep(30)
 
 def start_callback(xbee):
