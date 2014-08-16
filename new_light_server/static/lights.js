19bcdccdb094cab43e6421013fa2cceb889e6b07
Robert Diamond <rmd6502@gmail.com>
2014-08-15 22:14:16 -0400

    fix color updates

--- new_light_server/static/lights.js
+++ new_light_server/static/lights.js
@@ -44,31 +44,31 @@ function hidden_submit(form,control)
 function handleUpdates()
 {
     var xmlhttp = new XMLHttpRequest();
-    var loc = window.location;
-    loc.path="/query";
-    loc.search="";
+    var loc = String(window.location);
+    loc = loc.replace("lights/","query");
     
-    xmlhttp.open(loc, "GET", false);
+    xmlhttp.open("GET", loc, false);
     xmlhttp.send();
-    var result = eval(xmlhttp.responseText);
+    eval("var result = "+xmlhttp.responseText);
     if (!result) {
         return;
     }
-	for (var node in result["nodes"]) {
-        if (!node["name"]) {
-            continue;
+	for (var nodeid in result.nodes) {
+	var node = result.nodes[nodeid];
+      	if (!node.name) {
+       	    continue;
         }
-        var name = encodeURI(node["name"]);
-        if (node["color"]) {
-            var obj = document.getElementsById(name+"_color");
-            if (obj && jscolor.picker.owner != obj.color) {
-                obj.color.fromString(node["color"]);
+        var name = escape(node.name);
+        if (node.color) {
+            var obj = document.getElementById(name+"_color");
+            if (obj && (!jscolor.picker || jscolor.picker.owner != obj.color)) {
+                obj.color.fromString(node.color);
             }
         }
-        if (node["color2"]) {
-            var obj = document.getElementsById(name+"_color2");
-            if (obj && jscolor.picker.owner != obj.color) {
-                obj.color.fromString(node["color2"]);
+        if (node.color2) {
+            var obj = document.getElementById(name+"_color2");
+            if (obj && (!jscolor.picker || jscolor.picker.owner != obj.color)) {
+                obj.color.fromString(node.color2);
             }
         }
     }
@@ -76,7 +76,7 @@ function handleUpdates()
 
 function startUpdates()
 {
-    window.setInterval("handleUpdates()", 10000);
+    window.setInterval("handleUpdates()", 5000);
 }
 
 function setAllLights(color,form)
