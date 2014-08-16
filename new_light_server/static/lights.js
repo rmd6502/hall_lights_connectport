23188f4e031b0a57cc71a4a14f56118ddfee2b84
Robert Diamond <rmd6502@gmail.com>
2014-08-15 21:35:48 -0400

    update colors

--- new_light_server/static/lights.js
+++ new_light_server/static/lights.js
@@ -41,6 +41,44 @@ function hidden_submit(form,control)
     return false;
 }
 
+function handleUpdates()
+{
+    var xmlhttp = new XMLHttpRequest();
+    var loc = window.location;
+    loc.path="/query";
+    loc.search="";
+    
+    xmlhttp.open(loc, "GET", false);
+    xmlhttp.send();
+    var result = eval(xmlhttp.responseText);
+    if (!result) {
+        return;
+    }
+	for (var node in result["nodes"]) {
+        if (!node["name"]) {
+            continue;
+        }
+        var name = encodeURI(node["name"]);
+        if (node["color"]) {
+            var obj = document.getElementsById(name+"_color");
+            if (obj && jscolor.picker.owner != obj.color) {
+                obj.color.fromString(node["color"]);
+            }
+        }
+        if (node["color2"]) {
+            var obj = document.getElementsById(name+"_color2");
+            if (obj && jscolor.picker.owner != obj.color) {
+                obj.color.fromString(node["color2"]);
+            }
+        }
+    }
+}
+
+function startUpdates()
+{
+    window.setInterval("handleUpdates()", 10000);
+}
+
 function setAllLights(color,form)
 {
     var inputs = form.getElementsByTagName("INPUT");
